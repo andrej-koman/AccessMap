a5fcf521d96b22cf3418efdb3bf713b4b36cdf86
andrej-koman <andrej.koman123@gmail.com>
2025-01-18 14:13:59 +0100

    feat: Basic schema for the db

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,11 +5,21 @@ export default function MapComponent({ apiKey }: { apiKey: string }) {
   return (
     <APIProvider apiKey={apiKey}>
       <Map
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "100vw", height: "100vh", position: "absolute" }}
         defaultCenter={{ lat: 46.55339352380859, lng: 15.647246427534268 }}
-        defaultZoom={14}
-        gestureHandling={"greedy"}
+        defaultZoom={15}
         disableDefaultUI={true}
+        colorScheme="dark"
+        reuseMaps={true}
+        mapTypeId={"roadmap"}
+        styles={
+          [
+            {
+              featureType: "poi.business",
+              stylers: [{ visibility: "off" }]
+            }
+          ]
+        }
       />
     </APIProvider>
   );
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -12,8 +12,63 @@ import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
  */
 export const createTable = sqliteTableCreator((name) => `AccessMap_${name}`);
 
-export const posts = createTable(
-  "post",
+export const user = createTable(
+  "user",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    token: text("token", { length: 256 }).unique().notNull(),
+    disabilityId: int("disability_id", { mode: "number" }).notNull(),
+    expirationDate: int("expiration_date", { mode: "timestamp" }).notNull().$default(
+      () => new Date(Date.now() + 1000 * 60 * 60 * 24 * 365)
+    ),
+    roleId: int("role_id", { mode: "number" }).notNull(),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`(unixepoch())`)
+      .notNull(),
+    updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    )
+  }
+)
+
+export const marker = createTable(
+  "marker",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 256 }),
+    lat: int("lat", { mode: "number" }),
+    lng: int("lng", { mode: "number" }),
+    disabilityId: int("disability_id", { mode: "number" }).notNull(),
+    markerType: text("marker_type", { length: 256 }), // This will be an enum
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`(unixepoch())`)
+      .notNull(),
+    updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    )
+  }
+)
+
+export const markerDisability = createTable(
+  "marker_disability",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    markerId: int("marker_id", { mode: "number" }).notNull(),
+    disabilityId: int("disability_id", { mode: "number" }).notNull()
+  }
+)
+
+export const userDisability = createTable(
+  "user_disability",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    userId: int("user_id", { mode: "number" }).notNull(),
+    disabilityId: int("disability_id", { mode: "number" }).notNull()
+  }
+)
+
+export const disability = createTable(
+  "disability",
   {
     id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
     name: text("name", { length: 256 }),
@@ -22,9 +77,20 @@ export const posts = createTable(
       .notNull(),
     updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
       () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+    )
+  }
+)
+
+export const role = createTable(
+  "role",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 256 }),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`(unixepoch())`)
+      .notNull(),
+    updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    )
+  }
+)
